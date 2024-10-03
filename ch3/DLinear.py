1c11cc4899af0048cd0f0b44b65465d791bb0c18
Dongbin Kim <143064606+dongbeank@users.noreply.github.com>
2024-10-03 17:02:56 +0900

    Update DLinear.py

--- ch3/DLinear.py
+++ ch3/DLinear.py
@@ -76,35 +76,6 @@ class Model(nn.Module):
         # Encoder
         return self.encoder(x_enc)
 
-    def imputation(self, x_enc):
-        # Encoder
-        return self.encoder(x_enc)
-
-    def anomaly_detection(self, x_enc):
-        # Encoder
-        return self.encoder(x_enc)
-
-    def classification(self, x_enc):
-        # Encoder
-        enc_out = self.encoder(x_enc)
-        # Output
-        # (batch_size, seq_length * d_model)
-        output = enc_out.reshape(enc_out.shape[0], -1)
-        # (batch_size, num_classes)
-        output = self.projection(output)
-        return output
-
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
-        if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
-            dec_out = self.forecast(x_enc)
-            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
-        if self.task_name == 'imputation':
-            dec_out = self.imputation(x_enc)
-            return dec_out  # [B, L, D]
-        if self.task_name == 'anomaly_detection':
-            dec_out = self.anomaly_detection(x_enc)
-            return dec_out  # [B, L, D]
-        if self.task_name == 'classification':
-            dec_out = self.classification(x_enc)
-            return dec_out  # [B, N]
-        return None
+        dec_out = self.forecast(x_enc)
+        return dec_out[:, -self.pred_len:, :]  # [B, L, D]

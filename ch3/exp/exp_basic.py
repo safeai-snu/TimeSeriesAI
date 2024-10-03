5a47de770e7a8bc6c3a3993361509100f9250666
Dongbin Kim <143064606+dongbeank@users.noreply.github.com>
2024-10-03 22:05:58 +0900

    Update exp_basic.py

--- ch3/exp/exp_basic.py
+++ ch3/exp/exp_basic.py
@@ -22,8 +22,7 @@ class Exp_Basic(object):
 
     def _acquire_device(self):
         if self.args.use_gpu:
-            os.environ["CUDA_VISIBLE_DEVICES"] = str(
-                self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
             device = torch.device('cuda:{}'.format(self.args.gpu))
             print('Use GPU: cuda:{}'.format(self.args.gpu))
         else:

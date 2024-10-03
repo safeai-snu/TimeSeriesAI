041f9db8e893a43047dff4f2f9c35d62927f7575
Dongbin Kim <143064606+dongbeank@users.noreply.github.com>
2024-10-03 21:34:32 +0900

    Update Crossformer.py

--- ch3/models/Crossformer.py
+++ ch3/models/Crossformer.py
@@ -17,7 +17,7 @@ class Model(nn.Module):
     """
     def __init__(self, configs):
         super(Model, self).__init__()
-        self.enc_in = configs.enc_in
+        self.enc_in = 7
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
         self.seg_len = 12
@@ -27,44 +27,44 @@ class Model(nn.Module):
         self.pad_in_len = ceil(1.0 * configs.seq_len / self.seg_len) * self.seg_len
         self.pad_out_len = ceil(1.0 * configs.pred_len / self.seg_len) * self.seg_len
         self.in_seg_num = self.pad_in_len // self.seg_len
-        self.out_seg_num = ceil(self.in_seg_num / (self.win_size ** (configs.e_layers - 1)))
-        self.head_nf = configs.d_model * self.out_seg_num
+        self.out_seg_num = ceil(self.in_seg_num / (self.win_size ** (3 - 1)))
+        self.head_nf = 64 * self.out_seg_num
 
         # Embedding
-        self.enc_value_embedding = PatchEmbedding(configs.d_model, self.seg_len, self.seg_len, self.pad_in_len - configs.seq_len, 0)
+        self.enc_value_embedding = PatchEmbedding(64, self.seg_len, self.seg_len, self.pad_in_len - configs.seq_len, 0)
         self.enc_pos_embedding = nn.Parameter(
-            torch.randn(1, configs.enc_in, self.in_seg_num, configs.d_model))
-        self.pre_norm = nn.LayerNorm(configs.d_model)
+            torch.randn(1, 7, self.in_seg_num,64))
+        self.pre_norm = nn.LayerNorm(64)
 
         # Encoder
         self.encoder = Encoder(
             [
-                scale_block(configs, 1 if l is 0 else self.win_size, configs.d_model, configs.n_heads, configs.d_ff,
-                            1, configs.dropout,
-                            self.in_seg_num if l is 0 else ceil(self.in_seg_num / self.win_size ** l), configs.factor
-                            ) for l in range(configs.e_layers)
+                scale_block(configs, 1 if l is 0 else self.win_size, 64, 4, 128,
+                            1, 0.1,
+                            self.in_seg_num if l is 0 else ceil(self.in_seg_num / self.win_size ** l),3
+                            ) for l in range(2)
             ]
         )
         # Decoder
         self.dec_pos_embedding = nn.Parameter(
-            torch.randn(1, configs.enc_in, (self.pad_out_len // self.seg_len), configs.d_model))
+            torch.randn(1, 7, (self.pad_out_len // self.seg_len), 64))
 
         self.decoder = Decoder(
             [
                 DecoderLayer(
                     TwoStageAttentionLayer(configs, (self.pad_out_len // self.seg_len), configs.factor, configs.d_model, configs.n_heads,
-                                           configs.d_ff, configs.dropout),
+                                           128, 0.1),
                     AttentionLayer(
-                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                        FullAttention(False, 3, attention_dropout=0.1,
                                       output_attention=False),
-                        configs.d_model, configs.n_heads),
+                        64,4),
                     self.seg_len,
-                    configs.d_model,
-                    configs.d_ff,
-                    dropout=configs.dropout,
-                    # activation=configs.activation,
+                    64,
+                    128,
+                    dropout=0.1,
+                    # activation='gelu',
                 )
-                for l in range(configs.e_layers + 1)
+                for l in range(2 + 1)
             ],
         )
 
